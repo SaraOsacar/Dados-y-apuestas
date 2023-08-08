c9e2b4e5b23c5381be196e2d55888840400ee11b
SaraOsacar <saraosacar@gmail.com>
2023-08-08 15:46:02 +0200

    fix some style

--- js/main.js
+++ js/main.js
@@ -22,13 +22,13 @@ const handleClickButton = (event) => {
   // si la usuaria acierta el número, gana el doble de lo que ha apostado
   if (userNumber === randomNumberResult) {
     const newScoreWin = parseInt(quantity.value) * 2 + 50;
-    result.innerHTML = 'Has ganado el doble de lo apostado :)';
+    result.innerHTML = 'Has ganado el doble de lo apostado 😀';
     balance.innerHTML = newScoreWin;
     console.log('new result:', newScoreWin);
   } else {
     //Cuando la jugadora introduzca un número que no coincida con el número aleatorio, pierde todo lo apostado
     const newScoreLost = 50 - parseInt(quantity.value);
-    result.innerHTML = 'Has perdido lo apostado :(';
+    result.innerHTML = 'Has perdido lo apostado 😞';
     balance.innerHTML = newScoreLost;
     console.log('newScoreLost:', newScoreLost);
   }
